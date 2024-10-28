d1a793d177c6ce0f9e34e08a762baa48e1f789b5
liumingye <8676207+liumingye@users.noreply.github.com>
2024-10-28 23:44:43 +0800

    更改默认字体大小

--- packages/backend/src/server/web/boot.js
+++ packages/backend/src/server/web/boot.js
@@ -127,7 +127,7 @@
 	}
 	//#endregion
 
-	const fontSize = localStorage.getItem('fontSize');
+	const fontSize = localStorage.getItem('fontSize') || 2;
 	if (fontSize) {
 		document.documentElement.classList.add('f-' + fontSize);
 	}
