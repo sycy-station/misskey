032c479dec9cf0175e4f86622c7fa002b67c4831
liumingye <8676207+liumingye@users.noreply.github.com>
2024-10-31 21:04:27 +0800

    refactor：通用getBgColor

---
+++ packages/frontend/src/scripts/get-bg-color.ts
@@ -0,0 +1,13 @@
+/*
+ * SPDX-FileCopyrightText: syuilo and misskey-project
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+import tinycolor from 'tinycolor2';
+export const getBgColor = (elem?: Element | null | undefined): string | null => {
+	if (elem == null) return null;
+	const { backgroundColor: bg } = window.getComputedStyle(elem);
+	if (bg && tinycolor(bg).getAlpha() !== 0) {
+		return bg;
+	}
+	return getBgColor(elem.parentElement);
+};
